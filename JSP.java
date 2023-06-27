932d836dabecedccd046f83574c9d6583aaec759
Acetilsalicilic <1Uncanalmas@gmail.com>
2023-06-27 15:55:30 -0700

    Data update

--- JSP.java
+++ JSP.java
@@ -2,6 +2,14 @@ import java.sql.*;
 
 class JSP {
 
+    public static void main(String args[]) {
+        
+        UpdateBD prueba = new UpdateBD();
+        
+        System.out.println(prueba.RegistrarVuelo("Codigo", "Origen", "Destino", "00/00/0000", 1));
+        
+    }
+    
 }
 
 abstract class InterBD {
@@ -42,10 +50,10 @@ abstract class InterBD {
     }
 
     protected String 
-    password = "",
+    password = "1Lapassword-",
     driver = "com.mysql.cj.jdbc.Driver", 
     user = "root", 
-    url = "jdbc:mysql://localhost:3306/reg@losdb";
+    url = "jdbc:mysql://localhost:3306/didesaa";
 
     protected Connection cx;
     protected PreparedStatement st;
@@ -58,13 +66,19 @@ class UpdateBD  extends InterBD {
         super();
     }
 
-    public int RegistrarVuelo () {
+    public int RegistrarVuelo (String cod_vue, String ori_vue,  String des_vue,  String dia_vue, int nda_vue) {
 
         try {
             
             abrirCX(cx);
 
-            st = cx.prepareStatement("INSERT INTO `MVuelos`(`cod_cue`, `ori_vue`, `des_vue`, `dia_vue`, `nda_vue`) VALUES ('?', '?', '?', '?', '?');");
+            st = cx.prepareStatement("INSERT INTO `MVuelos`(`cod_vue`, `ori_vue`, `des_vue`, `dia_vue`, `nda_vue`) VALUES ('?', '?', '?', '?', '?');");
+            
+            st.setString(1, cod_vue);
+            st.setString(2, ori_vue);
+            st.setString(3, des_vue);
+            st.setString(4, dia_vue);
+            st.setInt(5, nda_vue);
 
             st.execute();
 
